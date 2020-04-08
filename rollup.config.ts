bc532ddd114af1e0d1a35bf0a284e47717c060e0
Christian Stewart <christian@paral.in>
2020-04-07 21:34:47 -0700

    chore: remove unused camelcase from rollup config
    Signed-off-by: Christian Stewart <christian@paral.in>

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 
@@ -12,7 +11,6 @@ const libraryName = 'soyuz'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    // { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
